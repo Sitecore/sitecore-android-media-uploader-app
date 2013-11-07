6a4fcf719391fecdd2596a79265eaaeb40942a79
Kirill Zotin <kzotin@gmail.com>
2013-11-07 16:56:24 +0200

    Show drawer opened on first launch.

--- MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/MainActivity.java
+++ MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -69,6 +70,12 @@ public class MainActivity extends Activity implements SlidingNavigationFragment.
 
         if (savedInstanceState == null) {
             onNavigationItemSelected(POSITION_MEDIA_BROWSER);
+        }
+
+        boolean sideMenuShown = Prefs.from(this).getBool(R.string.key_side_menu_shown, false);
+        if (!sideMenuShown) {
+            mDrawerLayout.openDrawer(Gravity.LEFT);
+            Prefs.from(this).put(R.string.key_side_menu_shown, true);
         }
     }
 
