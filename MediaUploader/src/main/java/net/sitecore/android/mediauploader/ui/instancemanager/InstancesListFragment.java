a19a1d874c9ca1260888583a4078c2c43cf859b0
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2013-11-08 18:08:59 +0200

    Added actionbar icon for Instance Fragment.

--- MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/instancemanager/InstancesListFragment.java
+++ MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/instancemanager/InstancesListFragment.java
@@ -2,17 +2,23 @@ package net.sitecore.android.mediauploader.ui.instancemanager;
 
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
+import net.sitecore.android.mediauploader.R;
 import net.sitecore.android.mediauploader.provider.UploadMediaContract.Instances;
+import net.sitecore.android.mediauploader.ui.EditInstanceActivity;
 import net.sitecore.android.mediauploader.ui.ScFragment;
 
 import butterknife.InjectView;
@@ -39,7 +45,26 @@ public class InstancesListFragment extends ScFragment implements LoaderCallbacks
         mListView.setOnItemClickListener(this);
 
         getLoaderManager().initLoader(0, null, this);
+
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_instances_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_instance:
+                startActivity(new Intent(getActivity(), EditInstanceActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -60,5 +85,6 @@ public class InstancesListFragment extends ScFragment implements LoaderCallbacks
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        startActivity(new Intent(getActivity(), EditInstanceActivity.class));
     }
 }
