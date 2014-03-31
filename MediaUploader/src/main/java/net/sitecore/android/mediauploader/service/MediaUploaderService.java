842d8b6f89da3179354e8add653b19ed42bfad28
Kirill Zotin <kzotin@gmail.com>
2014-03-31 14:09:44 +0300

    Uncommented uploading notification.

--- MediaUploader/src/main/java/net/sitecore/android/mediauploader/service/MediaUploaderService.java
+++ MediaUploader/src/main/java/net/sitecore/android/mediauploader/service/MediaUploaderService.java
@@ -1,12 +1,16 @@
 package net.sitecore.android.mediauploader.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
+import net.sitecore.android.mediauploader.R;
+import net.sitecore.android.mediauploader.ui.MainActivity;
+import net.sitecore.android.sdk.api.UploadMediaRequestOptions;
 import net.sitecore.android.sdk.api.UploadMediaService;
 
 import static net.sitecore.android.sdk.api.internal.LogUtils.LOGD;
-
-;
 
 public class MediaUploaderService extends UploadMediaService {
 
@@ -17,19 +21,19 @@ public class MediaUploaderService extends UploadMediaService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         LOGD("MediaUploaderService.onStartCommand");
-//        UploadMediaRequestOptions options = intent.getParcelableExtra(EXTRA_UPLOAD_OPTIONS);
-//
-//        NotificationCompat.Builder builder = new Builder(this);
-//        PendingIntent showMainActivityIntent = PendingIntent.getActivity(this, 0,
-//                new Intent(this, MainActivity.class), 0);
-//
-//        builder.setContentTitle("Uploading ...")
-//                .setContentText(options.getFullUrl())
-//                .setContentIntent(showMainActivityIntent)
-//                .setSmallIcon(R.drawable.ic_launcher);
-//
-//        Notification n = builder.build();
-//        startForeground(NOTIFICATION_IN_PROGRESS, n);
+        UploadMediaRequestOptions options = intent.getParcelableExtra(EXTRA_UPLOAD_OPTIONS);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        PendingIntent showMainActivityIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        builder.setContentTitle("Uploading ...")
+                .setContentText(options.getFullUrl())
+                .setContentIntent(showMainActivityIntent)
+                .setSmallIcon(R.drawable.ic_launcher);
+
+        Notification n = builder.build();
+        startForeground(NOTIFICATION_IN_PROGRESS, n);
 
         return super.onStartCommand(intent, flags, startId);
     }
