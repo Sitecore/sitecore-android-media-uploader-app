5ea0ac638e5a1cc31ec73c824de3191fe0d93e28
Kirill Zotin <kzotin@gmail.com>
2014-04-15 14:13:06 +0300

    Old DB tests added.

---
+++ MediaUploader/src/androidTest/java/net/sitecore/android/mediauploader/provider/UploadMediaProviderInstancesTest.java
@@ -0,0 +1,171 @@
+package net.sitecore.android.mediauploader.provider;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.test.ProviderTestCase2;
+
+import net.sitecore.android.mediauploader.provider.UploadMediaContract.Instances;
+import net.sitecore.android.mediauploader.provider.UploadMediaContract.Instances.Query;
+
+public class UploadMediaProviderInstancesTest extends ProviderTestCase2<UploadMediaProvider> {
+
+    private final String NAME_SAMPLE = "name";
+    private final String URL_SAMPLE = "http://test.url";
+    private final String LOGIN_SAMPLE = "test_login";
+    private final String PASSWORD_SAMPLE = "test_password";
+    private final String DEFAULT_FOLDER_SAMPLE = "/sitecore/media library";
+
+    private final String UPDATED_NAME_SAMPLE = "updated_name";
+    private final String UPDATED_URL_SAMPLE = "updated_url";
+    private final String UPDATED_LOGIN_SAMPLE = "updated_login";
+    private final String UPDATED_PASSWORD_SAMPLE = "updated_password";
+    private final String UPDATED_DEFAULT_FOLDER_SAMPLE = "updated_folder";
+
+    private ContentResolver mContentResolver;
+
+    public UploadMediaProviderInstancesTest() {
+        super(UploadMediaProvider.class, UploadMediaContract.CONTENT_AUTHORITY);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mContentResolver = getMockContentResolver();
+    }
+
+    public ContentValues getSampleValue() {
+        ContentValues value = new ContentValues();
+        value.put(Instances.URL, URL_SAMPLE);
+        value.put(Instances.LOGIN, LOGIN_SAMPLE);
+        value.put(Instances.PASSWORD, PASSWORD_SAMPLE);
+        value.put(Instances.ROOT_FOLDER, DEFAULT_FOLDER_SAMPLE);
+
+        return value;
+    }
+
+    private void checkCursor(Cursor c, int count) {
+        assertNotNull(c);
+        assertEquals(count, c.getCount());
+    }
+
+    public void testInsert() {
+
+        mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+
+        Cursor c = mContentResolver.query(Instances.CONTENT_URI, null, null, null, null);
+        checkCursor(c, 1);
+    }
+
+    public void testInsertMultiple() {
+        ContentValues[] values = new ContentValues[3];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = getSampleValue();
+        }
+
+        mContentResolver.bulkInsert(Instances.CONTENT_URI, values);
+
+        Cursor c = mContentResolver.query(Instances.CONTENT_URI, null, null, null, null);
+        checkCursor(c, values.length);
+    }
+
+    public void testDelete() {
+        mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+
+        assertEquals(1, mContentResolver.delete(Instances.CONTENT_URI, null, null));
+
+        Cursor c = mContentResolver.query(Instances.CONTENT_URI, null, null, null, null);
+        checkCursor(c, 0);
+    }
+
+    public void testQuery() {
+        mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+
+        Cursor c = mContentResolver.query(Instances.CONTENT_URI, Query.PROJECTION, null, null, null);
+        checkCursor(c, 1);
+        if (!c.moveToFirst()) fail("Trying to read cursor but it is empty");
+
+        assertEquals(URL_SAMPLE, c.getString(Query.URL));
+        assertEquals(LOGIN_SAMPLE, c.getString(Query.LOGIN));
+        assertEquals(PASSWORD_SAMPLE, c.getString(Query.PASSWORD));
+        assertEquals(DEFAULT_FOLDER_SAMPLE, c.getString(Query.ROOT_FOLDER));
+    }
+
+    public void testUpdate() {
+        mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+
+        Cursor c = mContentResolver.query(Instances.CONTENT_URI, null, null, null, null);
+        checkCursor(c, 1);
+
+        ContentValues newValue = new ContentValues();
+        newValue.put(Instances.URL, UPDATED_URL_SAMPLE);
+        newValue.put(Instances.LOGIN, UPDATED_LOGIN_SAMPLE);
+        newValue.put(Instances.PASSWORD, UPDATED_PASSWORD_SAMPLE);
+        newValue.put(Instances.ROOT_FOLDER, UPDATED_DEFAULT_FOLDER_SAMPLE);
+
+        String selection = Instances.LOGIN + "='" + UPDATED_LOGIN_SAMPLE + "'";
+        assertEquals(1, mContentResolver.update(Instances.CONTENT_URI, newValue, selection, null));
+
+        c = mContentResolver.query(Instances.CONTENT_URI, Query.PROJECTION, null, null, null);
+        checkCursor(c, 1);
+        if (!c.moveToFirst()) fail("Trying to read cursor but it is empty");
+
+        assertEquals(UPDATED_URL_SAMPLE, c.getString(Query.URL));
+        assertEquals(UPDATED_LOGIN_SAMPLE, c.getString(Query.LOGIN));
+        assertEquals(UPDATED_PASSWORD_SAMPLE, c.getString(Query.PASSWORD));
+        assertEquals(UPDATED_DEFAULT_FOLDER_SAMPLE, c.getString(Query.ROOT_FOLDER));
+    }
+
+    public void testGetInstanceById() {
+        Uri resultUri = mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+        assertNotNull(resultUri);
+
+        Cursor cursor = mContentResolver.query(resultUri, Query.PROJECTION, null, null,
+                null);
+        checkCursor(cursor, 1);
+    }
+
+    public void testDeleteInstanceById() {
+        Uri resultUri = mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+        assertNotNull(resultUri);
+
+        int count = mContentResolver.delete(resultUri, null, null);
+        assertEquals(1, count);
+    }
+
+    public void testUpdateInstanceById() {
+        Uri resultUri = mContentResolver.insert(Instances.CONTENT_URI, getSampleValue());
+        assertNotNull(resultUri);
+
+        ContentValues values = new ContentValues();
+        values.put(Instances.URL, UPDATED_URL_SAMPLE);
+        values.put(Instances.LOGIN, UPDATED_LOGIN_SAMPLE);
+        values.put(Instances.PASSWORD, UPDATED_PASSWORD_SAMPLE);
+        values.put(Instances.ROOT_FOLDER, UPDATED_DEFAULT_FOLDER_SAMPLE);
+
+        int count = mContentResolver.update(resultUri, values, null, null);
+        assertEquals(1, count);
+
+        Cursor cursor = mContentResolver.query(resultUri, Query.PROJECTION, null, null,
+                null);
+        checkCursor(cursor, 1);
+        if (!cursor.moveToFirst()) fail("Trying to read cursor but it is empty");
+
+        assertEquals(UPDATED_URL_SAMPLE, cursor.getString(Query.URL));
+        assertEquals(UPDATED_LOGIN_SAMPLE, cursor.getString(Query.LOGIN));
+        assertEquals(UPDATED_PASSWORD_SAMPLE, cursor.getString(Query.PASSWORD));
+        assertEquals(UPDATED_DEFAULT_FOLDER_SAMPLE, cursor.getString(Query.ROOT_FOLDER));
+    }
+
+    public void testUnsupportedOperation() {
+        try {
+            mContentResolver.insert(Uri.withAppendedPath(Instances.CONTENT_URI, "/error"),
+                    getSampleValue());
+            fail("UnsupportedOperationException should be here");
+        } catch (Exception exception) {
+            assertEquals(true, exception instanceof UnsupportedOperationException);
+        }
+    }
+
+}
---
+++ MediaUploader/src/androidTest/java/net/sitecore/android/mediauploader/provider/UploadMediaProviderUploadsTest.java
@@ -0,0 +1,81 @@
+package net.sitecore.android.mediauploader.provider;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.test.ProviderTestCase2;
+
+import net.sitecore.android.mediauploader.model.UploadStatus;
+import net.sitecore.android.mediauploader.provider.UploadMediaContract.Uploads;
+
+public class UploadMediaProviderUploadsTest extends ProviderTestCase2<UploadMediaProvider> {
+
+    private ContentResolver mContentResolver;
+
+    public UploadMediaProviderUploadsTest() {
+        super(UploadMediaProvider.class, UploadMediaContract.CONTENT_AUTHORITY);
+    }
+
+    private ContentValues newUploadValues(String itemName) {
+        ContentValues values = new ContentValues();
+        values.put(Uploads.ITEM_NAME, itemName);
+        values.put(Uploads.FILE_URI, "http://test.com/image.png");
+        values.put(Uploads.STATUS, UploadStatus.IN_PROGRESS.name());
+
+        return values;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mContentResolver = getMockContentResolver();
+
+        mContentResolver.insert(Uploads.CONTENT_URI, newUploadValues("init item 1"));
+        mContentResolver.insert(Uploads.CONTENT_URI, newUploadValues("init item 2"));
+    }
+
+    public void testQuery() {
+        Cursor c = mContentResolver.query(Uploads.CONTENT_URI, null, null, null, null);
+
+        assertNotNull(c);
+        assertEquals(2, c.getCount());
+    }
+
+    public void testInsert() {
+        Uri itemUri = mContentResolver.insert(Uploads.CONTENT_URI, newUploadValues("new_item"));
+        assertEquals("content://net.sitecore.android.provider/uploads/3", itemUri.toString());
+
+        String id = Uploads.getUploadId(itemUri);
+        assertEquals("3", id);
+
+        String where = Uploads._ID + " = ?";
+
+        Cursor c = mContentResolver.query(Uploads.CONTENT_URI, null, where, new String[]{id}, null);
+        assertEquals(1, c.getCount());
+
+        if (!c.moveToFirst()) fail("Not empty cursor expected.");
+        String name = c.getString(c.getColumnIndex(Uploads.ITEM_NAME));
+        assertEquals("new_item", name);
+    }
+
+    public void testUpdate() {
+        ContentValues values = newUploadValues("updated");
+        String where = Uploads.ITEM_NAME + " = ?";
+        int updated = mContentResolver.update(Uploads.CONTENT_URI, values, where, new String[]{"init item 1"});
+        assertEquals(1, updated);
+    }
+
+    public void testDeleteAll() {
+        int rowsDeleted = mContentResolver.delete(Uploads.CONTENT_URI, null, null);
+        assertEquals(2, rowsDeleted);
+
+        Cursor c = mContentResolver.query(Uploads.CONTENT_URI, null, null, null, null);
+        assertEquals(0, c.getCount());
+    }
+
+    public void testDeleteById() {
+    }
+
+}
+
