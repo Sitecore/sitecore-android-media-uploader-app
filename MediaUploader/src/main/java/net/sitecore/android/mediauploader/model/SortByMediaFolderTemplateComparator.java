acb15b9054b1cca7e90d28fca47a5f88ffbecab5
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-04-16 14:14:00 +0300

    Fixed bug with wrong sort order.

--- MediaUploader/src/main/java/net/sitecore/android/mediauploader/model/SortByMediaFolderTemplateComparator.java
+++ MediaUploader/src/main/java/net/sitecore/android/mediauploader/model/SortByMediaFolderTemplateComparator.java
@@ -11,11 +11,11 @@ public class SortByMediaFolderTemplateComparator implements Comparator<ScItem> {
     @Override public int compare(ScItem lhs, ScItem rhs) {
         final String template1 = lhs.getTemplate();
         final String template2 = rhs.getTemplate();
-        if (template1.equals(template2)) return lhs.getDisplayName().compareTo(rhs.getDisplayName());
+        if (template1.equals(template2)) return lhs.getDisplayName().compareToIgnoreCase(rhs.getDisplayName());
 
         if (template1.equals(TEMPLATE_MEDIA_FOLDER)) return -1;
         if (template2.equals(TEMPLATE_MEDIA_FOLDER)) return 1;
 
-        return lhs.getDisplayName().compareTo(rhs.getDisplayName());
+        return lhs.getDisplayName().compareToIgnoreCase(rhs.getDisplayName());
     }
 }
