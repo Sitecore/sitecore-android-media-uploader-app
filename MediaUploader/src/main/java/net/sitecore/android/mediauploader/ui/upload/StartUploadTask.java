5529c952ffda0427b890f75281f7ff470998dc6e
Kirill Zotin <kzotin@gmail.com>
2014-04-25 18:11:03 +0300

    Fix not closed cursor.

--- MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/upload/StartUploadTask.java
+++ MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/upload/StartUploadTask.java
@@ -33,9 +33,8 @@ public class StartUploadTask extends AsyncTask<String, Void, Void> {
     @Override protected Void doInBackground(String... params) {
         ContentResolver contentResolver = mContext.getContentResolver();
         String uploadId = params[0];
-        try {
+        try (Cursor cursor = contentResolver.query(Uploads.buildUploadUri(uploadId), Query.PROJECTION, null, null, null)) {
             // load upload fields
-            Cursor cursor = contentResolver.query(Uploads.buildUploadUri(uploadId), Query.PROJECTION, null, null, null);
             if (cursor != null && cursor.moveToFirst()) {
                 String itemName = cursor.getString(Query.ITEM_NAME);
                 Uri fileUri = Uri.parse(cursor.getString(Query.FILE_URI));
