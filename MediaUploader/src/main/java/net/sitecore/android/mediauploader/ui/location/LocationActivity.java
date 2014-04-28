3d507ff096eeecff8a497bfb919d979c17456246
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-04-28 11:51:42 +0300

    Added necessary dependencies, added permissions.

---
+++ MediaUploader/src/main/java/net/sitecore/android/mediauploader/ui/location/LocationActivity.java
@@ -0,0 +1,15 @@
+package net.sitecore.android.mediauploader.ui.location;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.sitecore.android.mediauploader.R;
+
+public class LocationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
